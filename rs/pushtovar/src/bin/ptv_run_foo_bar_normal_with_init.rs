adc0c6f77a35ac7c94ae1566682e8bdc0e8db81f
pvillela <pvillela@gmail.com>
2023-02-20 20:18:08 -0500

    Added threading to main functions for Rust pushtovar.

--- rs/pushtovar/src/bin/ptv_run_foo_bar_normal_with_init.rs
+++ rs/pushtovar/src/bin/ptv_run_foo_bar_normal_with_init.rs
@@ -1,8 +1,15 @@
 use pushtovar::fs::foo_sfl;
 use pushtovar::startup::initialize;
+use std::thread;
 
 fn main() {
     initialize();
+
+    let handle1 = thread::spawn(move || {
+        foo_sfl();
+    });
+
+    let _ = handle1.join();
 
     foo_sfl();
 }
--- rs/pushtovar/src/bin/ptv_run_foo_bar_override.rs
+++ rs/pushtovar/src/bin/ptv_run_foo_bar_override.rs
@@ -1,7 +1,7 @@
-use std::sync::Arc;
-
 use pushtovar::fs::{foo_sfl, BarBfCfgInfo, FooSflCfgInfo, BAR_BF_CFG_SRC, FOO_SFL_CFG_SRC};
 use pushtovar::fwk::update_cfg_src_with_fn;
+use std::sync::Arc;
+use std::thread;
 
 fn main() {
     update_cfg_src_with_fn(&FOO_SFL_CFG_SRC, || {
@@ -11,6 +11,12 @@ fn main() {
     });
 
     update_cfg_src_with_fn(&BAR_BF_CFG_SRC, || Arc::new(BarBfCfgInfo { z: 99 }));
+
+    let handle1 = thread::spawn(move || {
+        foo_sfl();
+    });
+
+    let _ = handle1.join();
 
     foo_sfl();
 }
