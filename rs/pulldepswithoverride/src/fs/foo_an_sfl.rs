03c6644a6057f7cdca5148bb1b7bcbb9565f9b9a
pvillela <pvillela@gmail.com>
2023-03-17 13:47:28 -0400

    Rust pulldepswithoverride: tweaked foo_an_sfl.

--- rs/pulldepswithoverride/src/fs/foo_an_sfl.rs
+++ rs/pulldepswithoverride/src/fs/foo_an_sfl.rs
@@ -1,3 +1,4 @@
+use super::bar_an_bf;
 use common::config::{get_app_configuration, AppCfgInfo};
 use common::fs_data::{FooAIn, FooAOut, FooASflCfgInfo};
 use common::fs_util::foo_core;
@@ -8,8 +9,6 @@ use once_cell::sync::{Lazy, OnceCell};
 use std::time::Duration;
 use tokio::time::sleep;
 
-use super::bar_an_bf;
-
 type FooAnSflCfg = CfgRefCellRc<FooASflCfgInfo>;
 type FooAnSflCfgInfo = FooASflCfgInfo;
 type FooAnIn = FooAIn;
@@ -17,9 +16,8 @@ type FooAnOut = FooAOut;
 
 pub type FooAnSflCfgOvd = CfgOvd<FooASflCfgInfo>;
 
-#[derive(Clone)]
 pub struct FooAnSflDeps {
-    pub bar_a_bf: ArcPinFn<u64, String>,
+    pub bar_an_bf: ArcPinFn<u64, String>,
 }
 
 impl std::fmt::Debug for FooAnSflDeps {
@@ -30,7 +28,7 @@ impl std::fmt::Debug for FooAnSflDeps {
 
 pub async fn foo_an_sfl(input: FooAnIn) -> FooAnOut {
     let FooAnIn { sleep_millis } = input;
-    let d = &FOO_AN_SFL_DEPS;
+    let FooAnSflDeps { bar_an_bf: bar } = &FOO_AN_SFL_DEPS as &FooAnSflDeps;
     let (a, b) = {
         let cfg = FOO_AN_SFL_CFG.with(|c| c.get_cfg());
         let a = cfg.a.clone();
@@ -38,7 +36,7 @@ pub async fn foo_an_sfl(input: FooAnIn) -> FooAnOut {
         (a, b)
     };
     sleep(Duration::from_millis(sleep_millis)).await;
-    let bar_res = (d.bar_a_bf)(0).await;
+    let bar_res = bar(0).await;
     let res = foo_core(a, b, bar_res);
     FooAnOut { res }
 }
@@ -47,7 +45,7 @@ pub static FOO_AN_SFL_DEPS: Lazy<&FooAnSflDeps> = Lazy::new(|| {
     static_ref_with_override(
         FOO_AN_SFL_DEPS_OVERRIDE.get(),
         FooAnSflDeps {
-            bar_a_bf: arc_pin_async_fn(bar_an_bf),
+            bar_an_bf: arc_pin_async_fn(bar_an_bf),
         },
     )
 });
