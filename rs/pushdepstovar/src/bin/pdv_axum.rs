920260b7681b5948d1677ed9522bbf436e93d292
pvillela <pvillela@gmail.com>
2023-04-07 13:04:47 -0400

    Rust pushdepstovar: added pdv_axum binary.

---
+++ rs/pushdepstovar/src/bin/pdv_axum.rs
@@ -0,0 +1,18 @@
+use axum::{routing::post, Router};
+use common::web::axum_handler::handler_of;
+use pushdepstovar::{fs::foo_a_sfl, startup::init_a_refreshable};
+
+#[tokio::main]
+async fn main() {
+    init_a_refreshable();
+
+    let foo_a_sfl_hdlr = handler_of(foo_a_sfl);
+
+    let app = Router::new().route("/", post(foo_a_sfl_hdlr));
+
+    let addr = ([127, 0, 0, 1], 8080).into();
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
