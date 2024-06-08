e383812086ec8ee4e761bcedb4c422e954ec87fd
pvillela <pvillela@gmail.com>
2024-06-08 11:49:22 -0400

    Fixed cargo check issues and reverted to Axum 0.6 until breaking changes to 0.7 can be addressed.

--- rs/common/src/test_support/test_utils.rs
+++ rs/common/src/test_support/test_utils.rs
@@ -2,7 +2,7 @@ use once_cell::sync::Lazy;
 
 /// Overrides the value of a lazy static. This function is thread-unsafe and
 /// should only be used for test execution.
-pub unsafe fn override_lazy<T>(r: &Lazy<T>, ovd_fn: fn() -> T) {
+pub unsafe fn override_lazy<T>(r: &mut Lazy<T>, ovd_fn: fn() -> T) {
     let mr = r as *const Lazy<T> as *mut Lazy<T>;
     *mr = Lazy::new(ovd_fn);
 }
