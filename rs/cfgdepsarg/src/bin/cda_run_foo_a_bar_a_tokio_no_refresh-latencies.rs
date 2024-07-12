ec08cd16e32f404d099a7efad8efcb5d30f8e7f8
pvillela <pvillela@gmail.com>
2024-07-12 13:26:41 -0400

    Updated based on latest latency_trace.

--- rs/cfgdepsarg/src/bin/cda_run_foo_a_bar_a_tokio_no_refresh-latencies.rs
+++ rs/cfgdepsarg/src/bin/cda_run_foo_a_bar_a_tokio_no_refresh-latencies.rs
@@ -11,26 +11,28 @@ fn main() {
     // Set below value to "trace" to enable full library tracing.
     // set_var("RUST_LOG", "info");
 
-    let latencies = LatencyTrace::default().measure_latencies_tokio(|| async {
-        // Set env_logger only if `tracing_subsriber` hasn't pulled in `tracing_log` and already set a logger.
-        // Otherwise, setting a second logger would panic.
-        // _ = env_logger::try_init();
+    let latencies = LatencyTrace::activated_default()
+        .unwrap()
+        .measure_latencies_tokio(|| async {
+            // Set env_logger only if `tracing_subsriber` hasn't pulled in `tracing_log` and already set a logger.
+            // Otherwise, setting a second logger would panic.
+            // _ = env_logger::try_init();
 
-        println!("===== cda_run_foo_a_bar_a_tokio_no_cache =====");
+            println!("===== cda_run_foo_a_bar_a_tokio_no_cache =====");
 
-        run(RunIn {
-            make_foo_a_sfl: make_foo_a_sfl,
-            unit_time_millis: 1,
-            app_cfg_first_refresh_units: 10,
-            app_cfg_refresh_delta_units: 10,
-            app_cfg_refresh_count: 10,
-            per_call_sleep_units: 10,
-            increment_to_print: 33,
-            concurrency: 5,
-            repeats: 100,
-        })
-        .await;
-    });
+            run(RunIn {
+                make_foo_a_sfl: make_foo_a_sfl,
+                unit_time_millis: 1,
+                app_cfg_first_refresh_units: 10,
+                app_cfg_refresh_delta_units: 10,
+                app_cfg_refresh_count: 10,
+                per_call_sleep_units: 10,
+                increment_to_print: 33,
+                concurrency: 5,
+                repeats: 100,
+            })
+            .await;
+        });
 
     println!("\nLatency stats below are in microseconds");
     for (span_group, stats) in latencies.summary_stats() {
